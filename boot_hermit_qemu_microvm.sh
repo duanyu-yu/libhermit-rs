23797eb1f34e6c281a52f89382559ca4b4275962
Yu Duan <yu_duan@yahoo.com>
2021-09-05 23:10:18 +0200

    update shellscript for booting

--- boot_hermit_qemu_microvm.sh
+++ boot_hermit_qemu_microvm.sh
@@ -8,7 +8,7 @@ sudo qemu-system-x86_64 -M microvm,x-option-roms=off,pit=off,pic=off,rtc=off \
         -smp 1 \
         -m 64M \
         -serial stdio \
-        -kernel ~/RWTH/Bachelorarbeit/rusty-loader/target/x86_64-unknown-hermit-loader/debug/rusty-loader \
+        -kernel ~/RWTH/Bachelorarbeit/loader/target/x86_64-unknown-hermit-loader/debug/rusty-loader \
         -initrd ~/RWTH/Bachelorarbeit/rusty-hermit/target/x86_64-unknown-hermit/debug/hello_world \
         -enable-kvm \
         -cpu host \

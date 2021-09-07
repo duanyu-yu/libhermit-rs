c58a8f95fc83b40c21d0f4ff1965728125e7ea71
Yu Duan <yu_duan@yahoo.com>
2021-09-07 15:06:30 +0200

    update mmio layout

--- src/arch/x86_64/kernel/mmio.rs
+++ src/arch/x86_64/kernel/mmio.rs
@@ -3,6 +3,7 @@ use crate::arch::x86_64::mm::{paging};
 use crate::arch::x86_64::mm::{PhysAddr, VirtAddr};
 use crate::arch::x86_64::mm::paging::virt_to_phys;
 use core::{str};
+use alloc::vec::Vec;
 
 const MAGIC_VALUE: u32 = 0x74726976 as u32;
 
@@ -10,7 +11,7 @@ pub const MMIO_START: usize = 0x00000000c0000000 as usize;
 pub const MMIO_END: usize = 0x00000000c0000fff as usize;
 
 /// The Layout of MMIO Device 
-#[repr(C, align(32))]
+#[repr(C, align(4))]
 pub struct MMIO {
     magic_value: u32,
     version: u32,
@@ -18,10 +19,12 @@ pub struct MMIO {
     vendor_id: u32,
 
     host_features: u32, // legacy only
-    host_features_sel: [u32; 3], // legacy only
+    host_features_sel: u32, // legacy only
+    _reserved0: [u32; 2],
     guest_features: u32, // legacy only
     guest_features_sel: u32, // legacy only
-    guest_page_size: [u32; 2], // legacy only
+    guest_page_size: u32, // legacy only
+    _reserved1: u32,
     // device_features: u32, // non-legacy only
     // device_features_sel: u32, // non-legacy only 
     // driver_features: u32, // non-legacy only
@@ -33,13 +36,17 @@ pub struct MMIO {
 
     // queue_ready: u32, // non-legacy only
     queue_align: u32, // legacy only
-    queue_pfn: [u32; 4], // legacy only
+    queue_pfn: u32, // legacy only
+    _reserved2: [u32; 3],
     
-    queue_notify: [u32; 4],
+    queue_notify: u32,
+    _reserved3: [u32; 3],
 
     interrupt_status: u32,
-    interrupt_ack: [u32; 3], 
-    status: [u32; 36],
+    interrupt_ack: u32, 
+    _reserved4: [u32; 2],
+    status: u32,
+    _reserved5: [u32; 35],
 
     // queue_desc_low: u32, // non-legacy only
     // queue_desc_high: u32, // non-legacy only 
@@ -77,7 +84,7 @@ impl MMIO {
         self.queue_num_max
     }
 
-    fn queue_pfn(&self) -> [u32; 4] {
+    fn queue_pfn(&self) -> u32 {
         self.queue_pfn
     }
 
@@ -85,7 +92,7 @@ impl MMIO {
         self.interrupt_status
     }
 
-    fn status(&self) -> [u32; 36] {
+    fn status(&self) -> u32 {
         self.status
     }
 
@@ -101,25 +108,25 @@ impl MMIO {
         infoentry!("Vendor ID", "{:#X}", self.vendor_id);
 
         infoentry!("Host Features", "{:#X}", self.host_features);
-        infoentry!("Host Features selection", "{:#X?}", self.host_features_sel);
+        infoentry!("Host Features selection", "{:#X}", self.host_features_sel);
         infoentry!("Guest Features", "{:#X}", self.guest_features);
         infoentry!("Guest Features selection", "{:#X}", self.guest_features_sel);
-        infoentry!("Guest Page Size", "{:#X?}", self.guest_page_size);
+        infoentry!("Guest Page Size", "{:#X}", self.guest_page_size);
 
         infoentry!("Virtual queue index", "{:#X}", self.queue_sel);
         infoentry!("Maximum queue size", "{:#X}", self.queue_num_max);
         infoentry!("Virtual queue size", "{:#X}", self.queue_num);
 
         infoentry!("Queue alignment", "{:#X}", self.queue_align);
-        infoentry!("Physical page number", "{:#X?}", self.queue_pfn);
+        infoentry!("Physical page number", "{:#X}", self.queue_pfn);
 
-        infoentry!("Queue notifier", "{:#X?}", self.queue_notify);
+        infoentry!("Queue notifier", "{:#X}", self.queue_notify);
 
         infoentry!("Interrupt status", "{:#X}", self.interrupt_status);
-        infoentry!("Interrupt acknowledge", "{:#X?}", self.interrupt_ack);
-        infoentry!("Device status", "{:#X?}", self.status);
+        infoentry!("Interrupt acknowledge", "{:#X}", self.interrupt_ack);
+        infoentry!("Device status", "{:#X}", self.status);
 
-        infoentry!("Configuration space", "{:#X?}", self.config);
+        infoentry!("Configuration space", "{:#X?}", self.config());
 
         infofooter!();
     }

1f35e8fb7f6f81ae98c064a6b5bb8c5fed3ef62b
Yu Duan <yu_duan@yahoo.com>
2021-09-08 08:54:27 +0200

    ready to add init_device function for mmio

--- src/drivers/virtio/transport/mmio.rs
+++ src/drivers/virtio/transport/mmio.rs
@@ -4,3 +4,9 @@
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
+
+use crate::arch::kernel::mmio as kernel_mmio;
+
+pub fn init_device(mmio: &kernel_mmio::MMIO) {
+    
+}
